9d74efaf3bbe1d58abdccdeb2f79ad17a7d17f29
saequus <slava@spetsyian.com>
2020-06-16 22:46:09 +0300

    New workaround

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from bottle import route, run, template
 
-@route('/say-hello-to/<name>')
+@route('/say-hello-to/<name>/')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
